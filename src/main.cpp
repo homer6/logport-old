2e29c7471d7997bfe2ba0d6aa1c8b5bb85beb942
Steve Sperandeo <steve.sperandeo@altumo.com>
2017-11-17 05:30:10 +0000

    Tabs to spaces.

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,18 @@ using std::endl;
 
 int main( int argc, char **argv ){
 
-	string broker_address;
-	string topic_name;
+    string broker_address;
+    string topic_name;
 
-	if( argc != 3 ){
-		cout << "Usage: " << argv[0] <<	" <broker> <kafka-topic-name>" << endl;
-		cout << "Eg1: " << argv[0] <<	" 127.0.0.1:9092 my-topic" << endl;
-		cout << "Eg2: " << argv[0] <<	" 127.0.0.1:9092,192.168.0.1:9092 my-topic" << endl;
-		return 0;
-	}else{
-		topic_name = string( argv[2] );
-		broker_address = string( argv[1] );
-	}
+    if( argc != 3 ){
+        cout << "Usage: " << argv[0] << " <broker> <kafka-topic-name>" << endl;
+        cout << "Eg1: " << argv[0] <<   " 127.0.0.1:9092 my-topic" << endl;
+        cout << "Eg2: " << argv[0] <<   " 127.0.0.1:9092,192.168.0.1:9092 my-topic" << endl;
+        return 0;
+    }else{
+        topic_name = string( argv[2] );
+        broker_address = string( argv[1] );
+    }
 
     // Create the config
     Configuration config = {
